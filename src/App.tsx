16c057e5b45dd1a171b6eed38a5c8b8146f6ade6
Aamogh <120258212+aamoghS@users.noreply.github.com>
2025-04-05 20:22:32 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
-import NextPage from "./pages/NextPage";
-import Login from "./pages/Login";
+import WindowScr from "./pages/WindowScr";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-r from-indigo-500 to-purple-600 text-white">
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<NextPage />} />
+          <Route path = "/" element={<WelcomePage />} />
+          <Route path="/windowscr" element={<WindowScr />} />
         </Routes>
       </div>
     </Router>
